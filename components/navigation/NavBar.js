f505f4604df8038b8240bccd4ef043fc1b0a4efd
akshay-at-bluepineapple <akshay.vishwakarma@bluepineapple.com>
2022-09-16 10:53:37 +0530

    updated navbar

--- components/navigation/NavBar.js
+++ components/navigation/NavBar.js
@@ -21,44 +21,30 @@ function NavBar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="fixed w-full bg-white bg-opacity-0">
+    <div className={`fixed w-full bg-white bg-opacity-0 ${open?"bg-opacity-0":""}`}>
       <div className="items-center justify-between px-4 py-2 md:flex md:px-4">
+        
         {/* Logo Section */}
         <div className="font-bold text-2xl cursor-pointer flex items-baseline font-[Poppins]  text-black">
           <Image src={logo} height={25} width={25} />
-          <span className="text-blue-800">Bluepineapple</span>
+          <span className="text-white">Bluepineapple</span>
         </div>
 
         {/* HamBurger Icon Section */}
         <div
           role="button"
-          tabIndex={-1}
           onClick={() => setOpen(!open)}
-          className="absolute text-3xl cursor-pointer right-4 top-3 md:hidden"
-        >
-          {open ? (
-            <XMarkIcon className="w-6 align-middle" />
-          ) : (
-            <Bars3Icon className="w-6" />
-          )}
+          className="absolute text-3xl cursor-pointer right-4 top-3 md:hidden">
+          {open ? (<XMarkIcon className="w-6 align-middle bg-white" />) : (<Bars3Icon className="w-6 bg-white" />)}
         </div>
 
-        {/* mid Section */}
+        {/* Nav Section */}
         <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-4 ${
-            open ? "top-10 " : "top-[-490px]"
-          }`}
-        >
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white bg-opacity-0 md:z-auto z-[10] left-0 w-full md:w-auto md:pl-0 pl-4 ${open ? "top-10 bg-opacity-50" : "top-[-490px] bg-opacity-0"}`}>
           {Links.map((link) => (
-            <li key={link.name} className="text-m md:ml-8 md:my-0 my-7">
+            <li key={link.name} className="text-m md:ml-8 md:my-0 my-7" onClick={() => setOpen(!open)}>
               <Link href={link.link}>
-                <a
-                  className={`font-semibold hover:text-blue-700 ${
-                    router.pathname === link.link
-                      ? "text-blue-700"
-                      : "text-black"
-                  }`}
-                >
+                <a className={`font-semibold hover:text-blue-700 ${router.pathname === link.link? "text-blue-700 font-extrabold": "text-white"}`} >
                   {link.name}
                 </a>
               </Link>
--- pages/Contact.js
+++ pages/Contact.js
@@ -1,41 +1,33 @@
-import React from 'react'
+import React from "react";
 
 function Contact() {
   return (
     <>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
+      <div
+        style={{
+          backgroundImage: `url("https://images.unsplash.com/photo-1661961110144-12ac85918e40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")`,
+          objectFit:"cover",
+          
+        }}
+        className="h-screen w-scree"
+      >
+      </div>
+      <div
+        style={{
+          backgroundImage: `url("https://images.unsplash.com/photo-1550859492-d5da9d8e45f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")`,
+        }}
+        className="h-screen w-scree"
+      >
+      </div>
+      <div
+        style={{
+          backgroundImage: `url("https://images.unsplash.com/photo-1535868463750-c78d9543614f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80")`,
+        }}
+        className="h-screen w-scree"
+      >
+      </div>
     </>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
