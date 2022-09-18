44a76fd37fc25f97e7e6f4b84778f528f94eb9dc
Soumya0399 <soumya.singh@bluepineapple.io>
2022-09-18 20:19:50 +0530

    Screen4

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import NavBar from '../components/navigation/NavBar';
+import Screen4 from '../components/HomePage/Screen4';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <NavBar />
       <Component {...pageProps} />
+      <Screen4 />
     </>
   );
 }
