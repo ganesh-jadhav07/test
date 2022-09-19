06323b2a743fe42879114c0fb70133c0604ed0b7
ganesh-jadhav07 <ganesh.jadhav@bluepineapple.io>
2022-09-20 00:16:31 +0530

    .

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CloudServices from "../components/Home/CloudServices";
 import InnovationServicesScreen from "../components/Home/InnovationServicesScreen";
-// import Products from "../components/Home/Products";
+import Products from "../components/Home/Products";
 import CustomerReviews from "../components/Home/CustomerReviews";
 
 function Home() {
@@ -9,7 +9,7 @@ function Home() {
     <div>
       <CloudServices />
       <InnovationServicesScreen />
-      {/* <Products /> */}
+      <Products />
       <CustomerReviews />
     </div>
   );
