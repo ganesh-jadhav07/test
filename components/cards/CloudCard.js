9215f1ab7b9d2c9fd49170bbd2827dadc8abb297
ganesh-jadhav07 <ganesh.jadhav@bluepineapple.io>
2022-09-19 14:40:23 +0530

    Container with SVG background

--- components/cards/CloudCard.js
+++ components/cards/CloudCard.js
@@ -13,16 +13,6 @@ function CloudCard({ ...cloud }) {
         </div>
       </div>
     </div>
-    // <div className={`h-72 w-72 rotate-45 ${classes.container}`}>
-    // <div className="p-4 h-72 w-72 -rotate-45">
-    //   <div className="flex justify-center mb-2">
-    //     <img src={cloud.img} alt="" className="w-20 mb-2 -mt-6" />
-    //   </div>
-    //   <div className="text-center">
-    //     <p>{cloud.text}</p>
-    //   </div>
-    // </div>
-    // </div>
   );
 }
 
