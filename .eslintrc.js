ed9cd025e415355c111562460d7d2b7eda3d10e4
ganesh-jadhav07 <ganesh.jadhav@bluepineapple.io>
2022-09-19 23:09:29 +0530

    removed airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,38 +3,38 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  extends: ["plugin:react/recommended"],
   overrides: [],
   parserOptions: {
     ecmaFeatures: {
-      jsx: 'false',
+      jsx: "false",
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['react', 'prettier'],
+  plugins: ["react"],
   rules: {
-    indent: ['error', 2],
-    'jsx-quotes': ['warn', 'prefer-double'],
+    indent: ["error", 2],
+    "jsx-quotes": ["warn", "prefer-double"],
     // 'max-len': [
     //   'warn',
     //   {
     //     code: 80,
     //   },
     // ],
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'arrow-body-style': 'off',
-    'no-console': 0,
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
+    "arrow-body-style": "off",
+    "no-console": 0,
 
-    allowExpression: 'off',
-    'react/jsx-filename-extension': [
-      'error',
+    allowExpression: "off",
+    "react/jsx-filename-extension": [
+      "error",
       {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
       },
     ],
-    'jsx-a11y/click-events-have-key-events': 'off'
+    "jsx-a11y/click-events-have-key-events": "off",
   },
 };
