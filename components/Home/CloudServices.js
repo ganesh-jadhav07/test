bc35790412c35e8921d585c39cc22a10b834c883
ganesh-jadhav07 <ganesh.jadhav@bluepineapple.io>
2022-09-27 08:47:22 +0530

    Cloud Service added

--- components/Home/CloudServices.js
+++ components/Home/CloudServices.js
@@ -1,39 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { HomePageSection1 } from "../../lib/wordpress/api";
-import dataFetcher from "../../lib/wordpress/dataFetcher";
-import BlueBlock from "../../public/images/home/BlueBlock.png";
-import loading from "../../public/images/loading.gif";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 
+// CSS
 import classes from "./CloudServices.module.css";
-
-// Cloud Images
-import salesF from "../../public/images/home/SalesLogo.png";
 
 // Icons
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
-import { Router, useRouter } from "next/router";
 
 let count = 0;
+
 const CloudServices = (props) => {
-  console.log(props.services);
+  const data = props.services;
+  const content = data.page.homepage_customfields;
+  const icons = data.post.icons;
+  const services = data.services.nodes;
   const router = useRouter();
-  // const [services, setServices] = useState(null);
-  // const [content, setContent] = useState(null);
-  // const [icons, setIcons] = useState(null);
 
-  // const [service, setService] = useState(0);
-
-  // Fetching data
-  // useEffect(() => {
-  //   (async function Clouds() {
-  //     const res = await dataFetcher(HomePageSection1);
-  //     const allData = res.data;
-  //     // console.log(allData.post.icons.blueDiamond.sourceUrl);
-  //     setServices(allData.services.nodes);
-  //     setContent(allData.page.homepage_customfields);
-  //     setIcons(allData.post.icons);
-  //   })();
-  // }, []);
+  const [service, setService] = useState(0);
 
   // Learn More Button Action
   const learnMoreHandler = () => {
@@ -53,84 +36,71 @@ const CloudServices = (props) => {
     setService(count);
   };
 
-  // services !== null
-  //   ? services.map((service) => {
-  //       console.log(service);
-  //     })
-  //   : "";
-  // console.log(content);
-  // if (services === null || content === null) {
-  //   return (
-  //     <div className={classes.section}>
-  //       <img src={loading.src}></img>
-  //     </div>
-  //   );
-  // } else {
   return (
-    // <div
-    //   className={classes.section}
-    //   style={{
-    //     backgroundImage: `url(${
-    //       content !== null ? content.section1Bg.sourceUrl : ""
-    //     })`,
-    //   }}>
-    //   <div className={classes.content}>
-    //     <div className={classes.section1Title}>
-    //       <div className={classes.blueBlock}>
-    //         <img
-    //           src={icons !== null ? icons.blueDiamond.sourceUrl : ""}
-    //           alt="dot.png"
-    //         />
-    //       </div>
-    //       <p>{content !== null ? content.section1Title : ""}</p>
-    //     </div>
-    //     <div className={classes.section1Subtitle}>
-    //       {content !== null ? content.section1Subtitle : ""}
-    //     </div>
-    //     <div className={classes.section1Tagline}>
-    //       {content !== null ? content.section1Tagline : ""}
-    //     </div>
-    //     <div className={classes.section1Content}>
-    //       {content !== null ? content.section1Content : ""}
-    //     </div>
-    //     <div>
-    //       <button
-    //         type="button"
-    //         className={classes.btn}
-    //         onClick={learnMoreHandler}>
-    //         {content !== null ? content.section1Buttondata : ""}
-    //       </button>
-    //     </div>
-    //   </div>
-    //   <div className={classes.cloudSection}>
-    //     <div className={classes.cloud}>
-    //       <div className={classes.cloudContent}>
-    //         <div className={classes.cloudLogo}>
-    //           <img
-    //             src={
-    //               services !== null
-    //                 ? services[service].services.logo.sourceUrl
-    //                 : ""
-    //             }
-    //             alt="CloudLogo"
-    //           />
-    //         </div>
-    //         <div className={classes.cloudDescription}>
-    //           <div className={classes.leftIcon} onClick={handlePrev}>
-    //             <ChevronLeftIcon className={`justify-center`} />
-    //           </div>
-    //           <div>
-    //             {services !== null ? services[service].services.overview : ""}
-    //           </div>
-    //           <div className={classes.rightIcon} onClick={handleNext}>
-    //             <ChevronRightIcon className={`justify-center`} />
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-    <div className={classes.section}>Hiii...</div>
+    <div
+      className={classes.section}
+      style={{
+        backgroundImage: `url(${
+          content !== null ? content.section1Bg.sourceUrl : ""
+        })`,
+      }}>
+      <div className={classes.content}>
+        <div className={classes.section1Title}>
+          <div className={classes.blueBlock}>
+            <img
+              src={icons !== null ? icons.blueDiamond.sourceUrl : ""}
+              alt="dot.png"
+            />
+          </div>
+          <p>{content !== null ? content.section1Title : ""}</p>
+        </div>
+        <div className={classes.section1Subtitle}>
+          {content !== null ? content.section1Subtitle : ""}
+        </div>
+        <div className={classes.section1Tagline}>
+          {content !== null ? content.section1Tagline : ""}
+        </div>
+        <div className={classes.section1Content}>
+          {content !== null ? content.section1Content : ""}
+        </div>
+        <div>
+          <button
+            type="button"
+            className={classes.btn}
+            onClick={learnMoreHandler}>
+            {content !== null ? content.section1Buttondata : ""}
+          </button>
+        </div>
+      </div>
+      <div className={classes.cloudSection}>
+        <div className={classes.cloud}>
+          <div className={classes.cloudContent}>
+            <div className={classes.cloudLogo}>
+              <img
+                src={
+                  services !== null
+                    ? services[service].services.logo.sourceUrl
+                    : ""
+                }
+                alt="CloudLogo"
+              />
+            </div>
+            <div className={classes.cloudDescription}>
+              <div className={classes.leftIcon} onClick={handlePrev}>
+                <ChevronLeftIcon className={`justify-center`} />
+              </div>
+              <div>
+                {services !== null ? services[service].services.overview : ""}
+              </div>
+              <div className={classes.rightIcon} onClick={handleNext}>
+                <ChevronRightIcon className={`justify-center`} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    // <div className={classes.section}>Hiii...</div>
   );
 };
 
--- components/Home/Product.js
+++ components/Home/Product.js
@@ -75,8 +75,7 @@ function Product(props) {
     <div className={`h-screen ${styles.bgImage}`}>
       {/* Title section */}
       <div
-        className={`${styles.headerTopContent}  flex justify-center w-full pt-4`}
-      >
+        className={`${styles.headerTopContent}  flex justify-center w-full pt-4`}>
         <div className={`${styles.titleContent}  flex flex-col items-center `}>
           <span className={`${styles.title} flex flex-row items-center`}>
             {diamondLogo}
@@ -102,8 +101,7 @@ function Product(props) {
               onClick={handlePrev}
             />
             <div
-              className={`${styles.squareDiamondBody}  grid grid-cols-1 justify-evenly `}
-            >
+              className={`${styles.squareDiamondBody}  grid grid-cols-1 justify-evenly `}>
               <div className="flex items-center justify-center">
                 <img
                   src="https://s3-alpha-sig.figma.com/img/d306/1d86/284cb069f62e8fb441c140e46f8bcae7?Expires=1664150400&Signature=fwubHvqif68DftC7fj~06UI3TGc4VDlZx1MTccOZt48hP5Za3kfFfUu8QBz1ClYmfGUY7QyQ1GycB4Co4yLO7aNQOu99acmuanM6MsLtTd~Ro-T4aBtPWzt59ciRShWDr47w0eoAKOeR~4FMlWQ5wtGHVAF7DfLhXdciBsA60z3D51S~8K1lxqTPVQnZr2P0nW2mEFkC1I4m8GvEShOMDItu8JA208EKmSeI2Kxa5mbpYbbI-lb9RhuqAAkV-nCw4MmqZlvDLkwX10jsvRsdko1kXRrL0oNisHWPEiT5xoaRLmY1Y2sjiNANCpu1E1kvSExqhnTK0iM2j0-6wHlb5w__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
@@ -111,13 +109,11 @@ function Product(props) {
                 />
               </div>
               <div
-                className={`${styles.diamondParagraph} text-center flex items-center overflow-hidden`}
-              >
+                className={`${styles.diamondParagraph} text-center flex items-center overflow-hidden`}>
                 {products[currentProduct].products.overview}
               </div>
               <span
-                className={`${styles.diamondBottom} text-center flex flex-col items-center justify-around `}
-              >
+                className={`${styles.diamondBottom} text-center flex flex-col items-center justify-around `}>
                 <button className={`${styles.learnMoreButton}`}>
                   {titleContent.section3Buttondata}
                 </button>
@@ -153,8 +149,7 @@ function Product(props) {
         </div>
         {/* right side content */}
         <div
-          className={`${styles.secondContent}  flex items-center justify-end `}
-        >
+          className={`${styles.secondContent}  flex items-center justify-end `}>
           <div className={`${styles.secondBody}  `}>
             {diamondLogo}
             <div className={`${styles.firstParagraph}`}>
