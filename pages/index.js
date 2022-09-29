942e2ae5801347160cf5607cc24e499302287fac
akshay-at-bluepineapple <akshay.vishwakarma@bluepineapple.com>
2022-09-29 23:10:47 +0530

    .

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
-import Home from './Home'
+import Home from "./Home";
 export default function Home1() {
-    return <Home/>
-
+  return <Home />;
 }
+
+fetch("http://20.193.137.229/?graphql", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    query: `mutation LoginUser { 
+      login( input: { 
+        clientMutationId: "uniqueId", 
+        username: "bluepineapple_user", 
+        password: "Bluepineapple@123" 
+      } ) { 
+        refreshToken 
+      } 
+    } 
+    `,
+  }),
+})
+  .then((res) => res.json())
+  .then((res) => console.log(res.data));
